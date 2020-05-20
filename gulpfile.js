b0137312780347f6142dbbbc3aabd695c06f4053
Carlos Diaz <charliedn@gmail.com>
2020-05-20 17:19:10 +0100

    add subtle animation to the foam in the theader

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,10 @@ gulp.task('watch-images', ['images-rebuild'], function () {
 gulp.task('sass-rebuild', function () {
   var plugins = [
     autoprefixer({ browsers: ['last 2 version'] }),
-    cssnano({ zindex: false })
+    cssnano({
+      zindex: false,
+      reduceIdents: false
+    })
   ];
   return gulp.src('_assets/sass/**/site.scss')
     .pipe(sourcemaps.init())
